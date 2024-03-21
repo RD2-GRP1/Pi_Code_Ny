5634ce014d88f2e762d652d40167d7e857bf8466
Kasper <kasper@ubuntu.myguest.virtualbox.org>
2024-03-21 13:42:17 +0100

    test

---
+++ Semesterprojekt/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <wiringPi.h>
+
+
+int main(void)
+{
+    // Red LED: Physical pin 18, BCM GPIO24, and WiringPi pin 5.
+    const int led = 5;
+
+    wiringPiSetup();
+
+    pinMode(led, OUTPUT);
+
+    while (1) {
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+        delay(500);
+    }
+
+    return 0;
+}
