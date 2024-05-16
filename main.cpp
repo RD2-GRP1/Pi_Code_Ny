dea012b3991ac13b69681ddc472b212a39eaeb1b
Anders Kristensen <161459848+Flenaflux@users.noreply.github.com>
2024-05-16 12:46:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,66 @@
+#include <QtWidgets>
 #include <QTcpSocket>
-#include <QHostAddress>
 
-void sendURScriptCommand(const QString& command) {
-    QTcpSocket socket;
-    socket.connectToHost(QHostAddress("ROBOT_IP"), 30002); // Skift ROBOT_IP og porten om nødvendigt
-    if(socket.waitForConnected()) {
-        socket.write(command.toUtf8());
-        socket.waitForBytesWritten();
-        socket.disconnectFromHost();
-    } else {
-        qDebug() << "Unable to connect to the robot.";
+class URClient : public QWidget {
+    Q_OBJECT
+
+public:
+    URClient(QWidget *parent = nullptr) : QWidget(parent) {
+        socket = new QTcpSocket(this);
+        connect(socket, &QTcpSocket::connected, this, &URClient::connected);
+        connect(socket, &QTcpSocket::disconnected, this, &URClient::disconnected);
+        connect(socket, &QTcpSocket::readyRead, this, &URClient::readyRead);
+
+        connectBtn = new QPushButton("Connect", this);
+        connect(connectBtn, &QPushButton::clicked, this, &URClient::connectToServer);
+
+        moveBtn = new QPushButton("Move", this);
+        connect(moveBtn, &QPushButton::clicked, this, &URClient::moveUR);
+
+        mainLayout = new QVBoxLayout(this);
+        mainLayout->addWidget(connectBtn);
+        mainLayout->addWidget(moveBtn);
+
+        setLayout(mainLayout);
     }
+
+private slots:
+    void connectToServer() {
+        socket->connectToHost("192.168.1.54", 29999); // Change IP address and port as per your setup
+    }
+
+    void connected() {
+        qDebug() << "Connected to server";
+    }
+
+    void disconnected() {
+        qDebug() << "Disconnected from server";
+    }
+
+    void readyRead() {
+        qDebug() << "Received data from server: " << socket->readAll();
+    }
+
+    void moveUR() {
+        // Send movement command to the UR robot
+        QByteArray data = "movel(p[1.0, 2.0, 3.0, 0.0, 0.0, 0.0], a=1.2, v=0.1)\n";
+        socket->write(data);
+    }
+
+private:
+    QTcpSocket *socket;
+    QPushButton *connectBtn;
+    QPushButton *moveBtn;
+    QVBoxLayout *mainLayout;
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    URClient client;
+    client.show();
+
+    return app.exec();
 }
 
-int main() {
-    // Eksempelkommando: Flyt til en bestemt position
-    QString urScriptCommand = "movej([0.1, -0.3, 0.5, -1.6, 1.5, 0.8], a=0.5, v=0.1)\n";
-
-    // Send URScript-kommandoen til robotten
-    sendURScriptCommand(urScriptCommand);
-
-    return 0;
-}
+#include "main.moc"
